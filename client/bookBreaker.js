f745f5c3956c0bc05e46d6f3fb1575d2453a5bbc
Rob Bruhn <rdbruhn@gmail.com>
2016-04-04 16:08:43 -0500

    Create calcDays, getRanges, and buildList functions in the breakFactory *Begin to implement breakBook method in breakController *Add ng-repeat to div that will display reading list data

--- client/bookBreaker.js
+++ client/bookBreaker.js
@@ -1,15 +1,27 @@
 angular.module('bookBreaker',[])
 .factory('breakFactory',function(){
   //add function to caculate days
-
+  var calcDays = function(dueDate){
+    //get difference between due date and today
+    
+  };
   //add function to divide pages by days
+  var getRanges = function(){
+    //calculate days per each day and return array of objects
 
+  };
   //add function to create list item array with ranges and dates
-  return {};
+  var buildList = function(dueDate, totalPages){
+    var totalDays = calcDays(dueDate);
+    return getRanges(totalDays, totalPages);
+  };
+  return { buildList:buildList };
 })
 .controller('breakController', function($scope, breakFactory){
   $scope.book = {};
   $scope.breakBook = function(){
     //send book data to factory and return array of check list objects
+  $scope.data = [];
+    breakFactory.buildList($scope.book.date, $scope.book.pages);
   };
 });
