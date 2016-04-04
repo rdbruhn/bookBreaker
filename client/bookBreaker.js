08ad96aa35053432e7951bf8c31caf910503d6e1
Rob Bruhn <rdbruhn@gmail.com>
2016-04-04 15:54:43 -0500

    Add div to display book data Add jQuery cdn and bookBreaker.js script tags to index.html Nest form in row div Add return object to breakFactory that will eventually hold methods

--- client/bookBreaker.js
+++ client/bookBreaker.js
@@ -1,14 +1,15 @@
-angular.modul('bookBreaker',[])
+angular.module('bookBreaker',[])
 .factory('breakFactory',function(){
   //add function to caculate days
 
   //add function to divide pages by days
 
   //add function to create list item array with ranges and dates
+  return {};
 })
 .controller('breakController', function($scope, breakFactory){
   $scope.book = {};
-  bookBreaker.breakBook = function(){
+  $scope.breakBook = function(){
     //send book data to factory and return array of check list objects
   };
 });
