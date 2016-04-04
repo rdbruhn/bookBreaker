16c1b9aa8240d2e351c4b7b54b19827f2ac751c9
Rob Bruhn <rdbruhn@gmail.com>
2016-04-04 17:04:31 -0500

    Implement calDays function to return total days *Implement getRanges function, needs fix to increment range *adjust buildList indentation *Add headers for displaying book information *Add moment.js to lib for later date formatting

--- client/bookBreaker.js
+++ client/bookBreaker.js
@@ -3,25 +3,45 @@ angular.module('bookBreaker',[])
   //add function to caculate days
   var calcDays = function(dueDate){
     //get difference between due date and today
-    
+    console.log("calcDays");
+    debugger
+    var today = new Date();
+    var due = dueDate;
+    var diff = (due - today)/1000;
+    diff = Math.abs(Math.floor(diff));
+    var days = Math.floor(diff/(24*60*60));
+    console.log(days);
+    return days;
   };
   //add function to divide pages by days
-  var getRanges = function(){
+  var getRanges = function(days, pages){
     //calculate days per each day and return array of objects
-
+    console.log("getRanges");
+    var results = [];
+    var page = 1;
+    var date, range;
+    var pagesPerDay = Math.floor(pages/days);
+    for(var day = 1; day <= days; day++){
+      endRange = page + pagesPerDay;
+      range = page + "-" + endRange;
+      date = moment().add(day,'days').calendar();
+      results.push({range: range, date: date});
+    }
+    return results;
   };
   //add function to create list item array with ranges and dates
   var buildList = function(dueDate, totalPages){
+    console.log("buildList");
     var totalDays = calcDays(dueDate);
-    return getRanges(totalDays, totalPages);
-  };
+      return getRanges(totalDays, totalPages);
+    };
   return { buildList:buildList };
 })
 .controller('breakController', function($scope, breakFactory){
   $scope.book = {};
   $scope.breakBook = function(){
     //send book data to factory and return array of check list objects
-  $scope.data = [];
-    breakFactory.buildList($scope.book.date, $scope.book.pages);
+    console.log("breakBook");
+  $scope.data = breakFactory.buildList($scope.book.dueDate, $scope.book.pages);
   };
 });
