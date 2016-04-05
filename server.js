7e865edcee78e632673f9a40ac10107746104e48
Rob Bruhn <rdbruhn@gmail.com>
2016-04-05 14:46:54 -0500

    Build placeholder post and get handling in server

--- server.js
+++ server.js
@@ -6,10 +6,33 @@ var port = process.env.PORT || 8000;
 
 //add database functionality later for storing users and reading plans
 
+//add express session
 app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(req,res){
   res.render('index');
+});
+
+app.post('#/signin', function(req,res){
+  //if !user create
+    //compare passwords
+      //request session
+});
+
+app.post('#/signup', function(req,res){
+  //if !user create
+    // if user redirect signin
+    //hash password
+    //store user data
+      //request session
+});
+
+app.get('/app', function(req,res){
+  //get lists from database
+});
+
+app.post('/app', function(req,res){
+  //store list in database
 });
 
 app.listen(port);
